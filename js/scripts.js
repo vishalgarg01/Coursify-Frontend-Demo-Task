3a43ce3adb3543957ad47ee45caeadec4c0868a2
vishalgarg01 <vishalgarghi@gmail.com>
2020-12-08 13:52:48 +0530

    front page

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+  
+});
+$(document).ready(function(){
+    $("#login").click(function(){
+      $("#loginModal").modal('show');
+    });
+ });
